688ffb2648c4024f7c6aff6c43ca8d5c5797a48c
Andy <38136612+andylebedev@users.noreply.github.com>
2023-10-08 17:29:05 +0200

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,1 +1,15 @@
-#tests for project.py
+from project import get_city, get_coordinates, get_weather
+import pytest
+
+def test_get_city():
+    with pytest.raises(TypeError):
+        get_city("ACB")
+
+def test_get_coordinates():
+    latitude, longitude = get_coordinates('Kyiv')
+    assert latitude == '50.4020'
+    assert longitude == '30.6148'
+
+def test_get_weather():
+    with pytest.raises(TypeError):
+        get_city(50,30)
